d98d8d9a66e3d940a46de6f0fce0b240b48b39c8
qasimq123 <45720227+qasimq123@users.noreply.github.com>
2025-02-23 14:55:02 +0000

    Create map.jsx

---
+++ langley-heritage/frontend/src/components/map.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+// Fix default marker issue in Leaflet
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+
+
+
+export default MyMap;
