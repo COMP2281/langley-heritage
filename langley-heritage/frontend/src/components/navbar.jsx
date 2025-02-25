589dbf7186191d7e25c92243d9307422c0fd82d5
Oscar Detnon <58948641+odetnon@users.noreply.github.com>
2025-02-25 15:09:33 +0000

    navbar and home page

--- langley-heritage/frontend/src/components/navbar.jsx
+++ langley-heritage/frontend/src/components/navbar.jsx
@@ -5,9 +5,14 @@ function NavBar() {
 		"relative pb-1 after:content-[''] after:absolute after:left-1/2 after:bottom-0 after:w-0 after:h-[2px] after:bg-white after:transition-all after:duration-300 after:origin-center hover:after:left-0 hover:after:w-full";
 	const links = ["/","/mission","contact","login"];
 	return (
-		<nav className="bg-[#780502] text-white p-8 flex justify-between items-center relative">
-			<div className="text-xl font-bold">
-				<span className="font-serif">Langley Heritage.</span>
+		<nav className="bg-[#780502] text-white pl-4 p-2 flex justify-between items-center relative">
+			<div className="flex items-center space-x-2 cursor-pointer" onClick={() => window.location.href = "/home"}>
+				<div className="text-xl">
+					<span className="font-serif">Langley</span>
+				</div>
+				<div className="text-xl font-bold">
+					<span className="font-serif">Heritage.</span>
+				</div>
 			</div>
 
 			<input type="checkbox" id="menu-toggle" className="peer hidden" />
@@ -18,9 +23,9 @@ function NavBar() {
 				☰
 			</label>
 
-			<ul className="hidden md:flex space-x-6 peer-checked:flex peer-checked:absolute peer-checked:top-full peer-checked:left-0 peer-checked:w-full peer-checked:bg-[#780502] peer-checked:flex-col peer-checked:items-center peer-checked:p-4 md:relative md:flex-row md:bg-transparent md:p-0">
+			<ul className="hidden md:flex space-x-6 peer-checked:flex peer-checked:absolute peer-checked:top-full peer-checked:left-0 peer-checked:w-full peer-checked:bg-[#780502] peer-checked:flex-row peer-checked:items-center peer-checked:p-4 md:relative md:flex-row md:bg-transparent md:p-0">
 				{["Home", "Our Mission", "Contact", "Login",""].map((item, index) => (
-					<li key={index} className="relative font-semibold text-center">
+					<li key={index} className="relative text-center">
 						<Link to={links[index]} className={centerUnderlineHover}>
 							{item}
 						</Link>
--- langley-heritage/frontend/src/components/pages/home.jsx
+++ langley-heritage/frontend/src/components/pages/home.jsx
@@ -3,6 +3,9 @@ import NavBar from "../navbar";
 function Home() {
   return (
     <>
+      <h1 className="font-serif font-bold text-2xl text-center p-8">
+        Explore Langley Park.
+      </h1>
       <p>Home</p>
     </>
   );
