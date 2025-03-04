25a328391ed11d93a4a77b5cf7980f16ba36d262
Oscar Detnon <58948641+odetnon@users.noreply.github.com>
2025-03-04 14:19:48 +0000

    contact form

--- langley-heritage/frontend/src/components/pages/contact.jsx
+++ langley-heritage/frontend/src/components/pages/contact.jsx
@@ -1,9 +1,13 @@
-function Contact(){
-    return (
-        <div className="flex flex-col justify-center items-center min-h-screen">
-            Contact
-	    </div>
-    );
-}
+import React from 'react';
+import ContactForm from '../components/contactform';
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <ContactForm />
+    </div>
+  );
+};
 
 export default Contact;
