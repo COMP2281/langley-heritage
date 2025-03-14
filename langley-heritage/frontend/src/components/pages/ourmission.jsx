0a24ebd0e782f135c0a3eac993f7ef4b9a3ed71a
pppp9612 <wenyan155>
2025-03-15 00:28:50 +0800

    Upload ourmission.jsx file

--- langley-heritage/frontend/src/components/pages/ourmission.jsx
+++ langley-heritage/frontend/src/components/pages/ourmission.jsx
@@ -1,10 +1,12 @@
+import missionImg from "../../../img/mission.jpg";
 
 function Mission(){
     return (
         <div className="flex flex-col justify-center items-center min-h-screen">
-            <h2 className="text-3xl font-bold font-serif text-[#780502]">
-                Our Mission
-            </h2>
+            {/* <h2 className="text-3xl font-bold font-serif text-[#780502]">
+                    Langley Park Cemetery
+            </h2> */}
+            <img src={missionImg} alt="Mission Img" style={{width: '60%'}} />
 	    </div>
     );
 }
